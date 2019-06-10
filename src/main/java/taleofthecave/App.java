60e2d858a40644f27e627f0c9beeb4b9c28e68bb
printnoname <printnoname@gmail.com>
2019-06-10 13:44:49 +0600

    initial commit

---
+++ src/main/java/taleofthecave/App.java
@@ -0,0 +1,13 @@
+package taleofthecave;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
